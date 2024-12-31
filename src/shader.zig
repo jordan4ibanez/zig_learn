54c00d37544899720ebee92145d7b68dbe78a84d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-31 01:38:11 -0500

    Create shader.zig

---
+++ src/shader.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const allocator = @import("allocator.zig");
+const gl = @import("gl");
+
+var database: std.StringHashMap(u32) = undefined;
+
+pub fn initialize() void {
+  database = std.StringHashMap(u32).init(allocator.get());
+}
+
+pub fn terminate() void {
+  // todo: make this thing clean the GPU memory.
+  database.clearAndFree();
+}
+
+//* PUBLIC API ==============================================
+
+pub fn use(name: *[]u8) void {
+    std.debug.print("{s}\n", .{name});
+}
