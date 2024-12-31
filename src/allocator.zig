f0a7250e3a46ed84b00788c29fb45b11ee039c9c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-30 22:31:25 -0500

    This is dumb, but I love it

--- src/allocator.zig
+++ src/allocator.zig
@@ -1,17 +1,25 @@
 // I am very lazy, and I am also learning, let me do this stupid shit. Thanks.
 const std = @import("std");
 
-var gpa = undefined;
-var allocator = undefined;
+var gpa: std.heap.GeneralPurposeAllocator(.{}) = undefined;
+var allocator: std.mem.Allocator = undefined;
 
 pub fn initialize() void {
     gpa = std.heap.GeneralPurposeAllocator(.{}){};
     allocator = gpa.allocator();
 }
 
-pub fn destroy() void {
+pub fn terminate() void {
     const deinit_status = gpa.deinit();
     if (deinit_status == .leak) {
         std.log.err("[Allocator]: Error, memory leak.", .{});
     }
+}
+
+pub fn create(comptime T: type) std.mem.Allocator.Error!*T {
+    return try allocator.create(T);
+}
+
+pub fn destroy(ptr: anytype) void {
+    allocator.destroy(ptr);
 }
