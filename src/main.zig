b78de46fff3b88a2095a72811250719d7089487e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-30 19:15:18 -0500

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,16 @@
 const std = @import("std");
+const glfw = @import("mach-glfw");
+
+fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
+    std.log.err("glfw: {}: {s}\n", .{ error_code, description });
+}
 
 pub fn main() !void {
     std.debug.print("hello\n", .{});
+
+    // glfw.setErrorCallback(errorCallback);
+
+    // if (!glfw.init()) {
+
+    // }
 }
