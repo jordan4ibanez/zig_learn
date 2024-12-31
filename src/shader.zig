507ef3747f42c498bd7d1e5b0b7886d2786700c2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-31 01:53:36 -0500

    gimme those locations

--- src/shader.zig
+++ src/shader.zig
@@ -4,7 +4,11 @@ const gl = @import("gl");
 
 var database: std.StringHashMap(u32) = undefined;
 
-const 
+const position_vbo_location = 0;
+const color_vbo_location = 1;
+
+const camera_matrix_uniform_location = 0;
+const object_matrix_uniform_location = 1;
 
 pub fn initialize() void {
     database = std.StringHashMap(u32).init(allocator.get());
@@ -19,4 +23,6 @@ pub fn terminate() void {
 
 pub fn new(name: []const u8, vert_path: []const u8, frag_path: []const u8) void {
     std.debug.print("{s}{s}{s}\n", .{ name, vert_path, frag_path });
+    
+        
 }
