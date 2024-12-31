fdeeecfe8680725ff56023b4e1530ef53ce98b82
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-30 19:26:53 -0500

    Add base code

--- src/main.zig
+++ src/main.zig
@@ -6,11 +6,24 @@ fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
 }
 
 pub fn main() !void {
-    std.debug.print("hello\n", .{});
+    std.debug.print("[Main]: Hello!\n", .{});
+    glfw.setErrorCallback(errorCallback);
 
-    // glfw.setErrorCallback(errorCallback);
+    if (!glfw.init(.{})) {
+        std.log.err("[GLFW] Error: Failed to initialize. {?s}", .{glfw.getErrorString()});
+    } else {
+        std.log.debug("[GLFW]: Successfully initialized.", .{});
+    }
+    defer glfw.terminate();
 
-    // if (!glfw.init()) {
+    const window: glfw.Window = glfw.Window.create(1024, 768, "Program", null, null, .{}) orelse {
+        std.log.err("[GLFW] Error: Failed to create glfw window. {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    };
+    defer window.destroy();
 
-    // }
+    while (!window.shouldClose()) {
+        window.swapBuffers();
+        glfw.pollEvents();
+    }
 }
