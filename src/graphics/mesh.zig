2e2978ede47ed5d555454039a32fbf0495708911
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-03 01:44:51 -0500

    position upload

--- src/graphics/mesh.zig
+++ src/graphics/mesh.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const gl = @import("gl");
 const allocator = @import("../utility/allocator.zig");
+const shader = @import("shader.zig");
 
 pub const Mesh = struct {
     vao: gl.uint,
@@ -21,8 +22,28 @@ pub fn terminate() void {
     database.clearAndFree();
 }
 
-//* PUBLIC API ==============================================
+//* PUBLIC API. ==============================================
 
-// pub fn new_mesh(positions: *[]f32, colors: *[]f32) void {
+pub fn new(positions: []f32, colors: []f32) void {
+    std.debug.print("{any}, {any}\n", .{ positions, colors });
 
-// }
+    const vboPosition = positionUpload(positions);
+
+    std.debug.print("{any}, {any}, {any}\n", .{ positions, colors, vboPosition });
+}
+
+//* INTERNAL API. ==============================================
+
+///
+/// Upload an array of positions into the GPU.
+///
+fn positionUpload(positions: []f32) gl.uint {
+    var vboPosition: gl.uint = 0;
+    gl.GenBuffers(1, (&vboPosition)[0..1]);
+    gl.BindBuffer(gl.ARRAY_BUFFER, vboPosition);
+    gl.BufferData(gl.ARRAY_BUFFER, @intCast(@sizeOf(f32) * positions.len), positions.ptr, gl.STATIC_DRAW);
+    gl.VertexAttribPointer(vboPosition, 3, gl.FLOAT, gl.FALSE, 0, 0);
+    gl.EnableVertexAttribArray(vboPosition);
+    gl.BindBuffer(gl.ARRAY_BUFFER, 0);
+    return vboPosition;
+}
