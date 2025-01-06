5336b1e861534dce06b826dbdca3fe08c85c8bb7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-05 22:10:30 -0500

    make a flow

--- src/graphics/texture.zig
+++ src/graphics/texture.zig
@@ -1,10 +1,18 @@
+const std = @import("std");
 const stbi = @import("zstbi");
 const allocator = @import("../utility/allocator.zig");
 
+var database: std.StringHashMap(u8) = undefined;
+
+//* ON/OFF SWITCH. ==============================================
+
 pub fn initialize() void {
-  
+    stbi.init(allocator.get());
+    _ = &database;
 }
 
 pub fn terminate() void {
-
+    stbi.deinit();
 }
+
+//* PUBLIC API. ==============================================
