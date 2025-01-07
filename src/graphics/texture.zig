81ace8d04af83b1b23bcee017ed40203e2cd5faa
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-07 04:20:44 -0500

    Some basic texture things with stb

--- src/graphics/texture.zig
+++ src/graphics/texture.zig
@@ -22,6 +22,27 @@ pub fn terminate() void {
 
 //* PUBLIC API. ==============================================
 
+///
+/// Create a texture from a file location.
+///
+/// This will take in your /path/to/image.png and create a new string as the key image.png.
+///
 pub fn new(location: []const u8) void {
     _ = &location;
+
+    const nullTerminatedLocation: [:0]const u8 = std.fmt.allocPrintZ(allocator.get(), "{s}", .{location}) catch |err| {
+        std.log.err("[Texture]: Failed to null terminate string {s}. {s}", .{ location, @errorName(err) });
+        std.process.exit(1);
+    };
+    defer allocator.free(nullTerminatedLocation);
+
+    var blah = stbi.Image.loadFromFile(nullTerminatedLocation, 4) catch |err| {
+        std.log.err("[Texture]: Failed to load texutre {s}. {s}", .{ location, @errorName(err) });
+        std.process.exit(1);
+    };
+    blah.deinit();
+
+    
+
+    _ = &blah;
 }
