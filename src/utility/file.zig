a515e72126d7cb951c217525117bc266abe14e1e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-07 04:10:10 -0500

    Fix this actual mess

--- src/utility/file.zig
+++ src/utility/file.zig
@@ -8,20 +8,20 @@ const allocator = @import("allocator.zig");
 ///
 pub fn readToString(location: []const u8) []const u8 {
     const codeFile = std.fs.cwd().openFile(location, .{}) catch |err| {
-        std.log.err("[Shader]: Failed to open file for {s}. {s}", .{ location, @errorName(err) });
+        std.log.err("[File]: Failed to open file for {s}. {s}", .{ location, @errorName(err) });
         std.process.exit(1);
     };
     defer codeFile.close();
 
     const fileSizeBytes = codeFile.getEndPos() catch |err| {
-        std.log.err("[Shader]: Failed to get file length for {s}. {s}", .{ location, @errorName(err) });
+        std.log.err("[File]: Failed to get file length for {s}. {s}", .{ location, @errorName(err) });
         std.process.exit(1);
     };
 
     const buffer: []u8 = allocator.alloc(u8, fileSizeBytes);
 
     _ = codeFile.readAll(buffer) catch |err| {
-        std.log.err("[Shader]: Failed to read file for {s}. {s}", .{ location, @errorName(err) });
+        std.log.err("[File]: Failed to read file for {s}. {s}", .{ location, @errorName(err) });
         std.process.exit(1);
     };
 
@@ -29,29 +29,34 @@ pub fn readToString(location: []const u8) []const u8 {
 }
 
 ///
-///  The easy way to read an entire file to a string buffer.
+/// The easy way to read an entire file to a string buffer.
 /// For talking to C.
 ///
-pub fn readToNullTerminatedString(location: []const u8) []const u8 {
+pub fn readToNullTerminatedString(location: []const u8) [:0]const u8 {
     const codeFile = std.fs.cwd().openFile(location, .{}) catch |err| {
-        std.log.err("[Shader]: Failed to open file for {s}. {s}", .{ location, @errorName(err) });
+        std.log.err("[File]: Failed to open file for {s}. {s}", .{ location, @errorName(err) });
         std.process.exit(1);
     };
+
     defer codeFile.close();
 
     const fileSizeBytes = codeFile.getEndPos() catch |err| {
-        std.log.err("[Shader]: Failed to get file length for {s}. {s}", .{ location, @errorName(err) });
+        std.log.err("[File]: Failed to get file length for {s}. {s}", .{ location, @errorName(err) });
         std.process.exit(1);
     };
 
-    const buffer: []u8 = allocator.alloc(u8, fileSizeBytes + 1);
+    const buffer: []u8 = allocator.alloc(u8, fileSizeBytes);
+    defer allocator.free(buffer);
 
     _ = codeFile.readAll(buffer) catch |err| {
-        std.log.err("[Shader]: Failed to read file for {s}. {s}", .{ location, @errorName(err) });
+        std.log.err("[File]: Failed to read file for {s}. {s}", .{ location, @errorName(err) });
         std.process.exit(1);
     };
 
-    buffer[fileSizeBytes] = 0;
+    const nullTerminatedBuffer = std.fmt.allocPrintZ(allocator.get(), "{s}", .{buffer}) catch |err| {
+        std.log.err("[File]: Failed to null terminate string for {s}. {s}", .{ location, @errorName(err) });
+        std.process.exit(1);
+    };
 
-    return buffer;
+    return nullTerminatedBuffer;
 }
