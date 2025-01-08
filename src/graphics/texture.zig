37936f4af43943a22df020467f14a3755f444d7a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-08 02:20:20 -0500

    Store texture

--- src/graphics/texture.zig
+++ src/graphics/texture.zig
@@ -3,14 +3,14 @@ const stbi = @import("zstbi");
 const gl = @import("gl");
 const allocator = @import("../utility/allocator.zig");
 
-var database: std.StringHashMap(u8) = undefined;
+var database: std.StringHashMap(gl.uint) = undefined;
 
 //* ON/OFF SWITCH. ==============================================
 
 pub fn initialize() void {
     stbi.setFlipVerticallyOnLoad(true);
     stbi.init(allocator.get());
-    database = std.StringHashMap(u8).init(allocator.get());
+    database = std.StringHashMap(gl.uint).init(allocator.get());
 }
 
 pub fn terminate() void {
@@ -52,5 +52,37 @@ pub fn new(location: []const u8) void {
         std.process.exit(1);
     }
 
-    // todo: add ID to database.
+    // Getting the file name here. //todo: This should probably be in a string library.
+
+    var indexOfFileName: usize = 0;
+    var stringIterator = std.mem.split(u8, location, "/");
+
+    while (stringIterator.next()) |val| {
+        _ = &val;
+        indexOfFileName += 1;
+    }
+
+    stringIterator.reset();
+
+    var fileName = allocator.alloc(u8, 0);
+
+    var j: usize = 0;
+    while (stringIterator.next()) |val| {
+        j += 1;
+
+        if (j == indexOfFileName) {
+            fileName = allocator.realloc(fileName, val.len);
+            @memcpy(fileName, val);
+        }
+    }
+
+    if (fileName.len == 0) {
+        std.log.err("[Texture]: Failed to find file name for {s}.", .{location});
+        std.process.exit(1);
+    }
+
+    database.putNoClobber(fileName, textureID) catch |err| {
+        std.log.err("[Texture]: Failed store texture {s}. {s}", .{ location, @errorName(err) });
+        std.process.exit(1);
+    };
 }
