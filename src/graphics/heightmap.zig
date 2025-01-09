1d4e6960cb122263213bd56b6db993df5ff80be8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-09 00:43:18 -0500

    Figure out byte data

--- src/graphics/heightmap.zig
+++ src/graphics/heightmap.zig
@@ -6,7 +6,6 @@ const allocator = @import("../utility/allocator.zig");
 pub const HeightMap = struct {
     width: u32,
     height: u32,
-
     data: [][]f32,
 };
 
@@ -18,25 +17,28 @@ pub fn load(location: []const u8) HeightMap {
     const nullTerminatedLocation = string.nullTerminate(location);
     defer allocator.free(nullTerminatedLocation);
 
-    var image = stbi.Image.loadFromFile(nullTerminatedLocation, 2) catch |err| {
+    var image = stbi.Image.loadFromFile(nullTerminatedLocation, 1) catch |err| {
         std.log.err("[Texture]: Failed to load texture {s}. {s}", .{ location, @errorName(err) });
         std.process.exit(1);
     };
     defer image.deinit();
 
-    std.debug.print("{d}, {d}, {d}, {d}\n", .{ image.width, image.height, image.data.len, image.bytes_per_component });
+    // std.debug.print("{d}, {d}, {d}, {d}\n", .{ image.width, image.height, image.data.len, image.bytes_per_component });
 
     const len = image.data.len / image.bytes_per_component;
     for (0..len) |i| {
         const index = i * image.bytes_per_component;
 
-        const boof = [2]u8{ image.data[index], image.data[index + 1] };
+        const byteData = [2]u8{ image.data[index], image.data[index + 1] };
+        const rawValue: u16 = std.mem.readInt(u16, &byteData, .little);
 
-        // todo: try memcpy hack here
-        const value: u16 = std.mem.readInt(u16, &boof, .big);
-        const fef: f16 = @bitCast(value);
+        // Heightmap data is of scalar [0.0 - 1.0]
+        const floatingLiteral: f32 = @floatFromInt(rawValue);
 
-        std.debug.print("{d}, {d}, {d}\n", .{ index, fef, image.num_components });
+        // const converted
+        _ = &floatingLiteral;
+
+        // std.debug.print("{s}, {d}, {d}, {d}, {d}\n", .{ location, rawBytes, index, floatingValue, image.num_components });
     }
 
     _ = &map;
