29e16c87452ca03bc3e61b662b0136ee2dd317d1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-13 04:11:22 -0500

    This was dumb

--- src/input/keyboard.zig
+++ src/input/keyboard.zig
@@ -1,19 +1,15 @@
 const std = @import("std");
 const glfw = @import("mach-glfw");
-const window = @import("../graphics/window.zig");
 
 //* PUBLIC API. ===========================================================
 
 ///
 /// Check if a key is down.
 ///
-pub fn isDown(key: glfw.Key) bool {
-    return (window.getKey(key) == glfw.Action.press);
-}
+// pub fn isDown(key: glfw.Key) bool {
+//     return (window.getKey(key) == glfw.Action.press);
+// }
 
 ///
 /// Check if a key is up.
 ///
-pub fn isUp(key: glfw.Key) bool {
-    return (window.getKey(key) == glfw.Action.release);
-}
