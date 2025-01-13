6c2b330371d12ad6f6041244102442e53a28b930
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-13 04:06:05 -0500

    Finish transitioning to camera api

--- src/main.zig
+++ src/main.zig
@@ -148,10 +148,8 @@ pub fn main() !void {
 
     texture.use("sand.png");
 
-    var rotation: f32 = -30;
-    var translation: f32 = 0;
-    var timer: f32 = 0.0;
-    var one = false;
+    const rotation: f32 = -30;
+    const translation: f32 = 0;
 
     while (!window.shouldClose()) {
         window.pollEvents();
@@ -160,39 +158,11 @@ pub fn main() !void {
         camera.clearColorBuffer();
         camera.clearDepthBuffer();
 
-        if (one) {
-            camera.setCameraPosition(10, 2, 0);
-            camera.setCameraRotation(-20.0, rotation);
-            camera.updateCameraMatrix();
+        camera.setCameraPosition(10, 2, 0);
+        camera.setCameraRotation(-20.0, rotation);
+        camera.updateCameraMatrix();
 
-            camera.updateObjectMatrix(0, 0, translation, 0, 0, 1.0);
-        } else {
-            var cameraMatrix = Mat4.perspective(65.0, window.getAspectRatio(), 0.1, 100.0);
-            cameraMatrix = cameraMatrix.rotate(180.0, Vec3.new(0, 1, 0));
-            cameraMatrix = cameraMatrix.rotate(-20, Vec3.new(1, 0, 0));
-            cameraMatrix = cameraMatrix.rotate(rotation, Vec3.new(0, 1, 0));
-            shader.setMat4Uniform(shader.CAMERA_MATRIX_UNIFORM_LOCATION, cameraMatrix);
-
-            var objectMatrix = Mat4.identity();
-            objectMatrix = objectMatrix.translate(Vec3.new(-10.0, -2.0, translation));
-            // objectMatrix = objectMatrix.rotate(rotation, Vec3.new(0, 1, 0));
-            objectMatrix = objectMatrix.scale(Vec3.new(1, 1, 1));
-            shader.setMat4Uniform(shader.OBJECT_MATRIX_UNIFORM_LOCATION, objectMatrix);
-        }
-
-        timer += 0.01;
-        rotation += 1;
-        
-        if (timer > 0.4) {
-            one = !one;
-            timer = 0.0;
-            if (one) {
-                std.debug.print("tick\n", .{});
-            } else {
-                std.debug.print("tock\n", .{});
-            }
-        }
-        translation -= 0.01;
+        camera.updateObjectMatrix(0, 0, translation, 0, 0, 1.0);
 
         mesh.draw("test");
 
