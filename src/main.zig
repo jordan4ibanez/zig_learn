0a7caf7161e9ba313fee8990b5a13982ebdef46c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-15 09:59:05 -0500

    Shut literally everything down

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,17 @@
 const std = @import("std");
-const gl = @import("gl");
+// const gl = @import("gl");
 const za = @import("zalgebra");
-const glfw = @import("mach-glfw");
-const stbi = @import("zstbi");
+// const glfw = @import("mach-glfw");
+// const stbi = @import("zstbi");
 const allocator = @import("utility/allocator.zig");
-const window = @import("graphics/window.zig");
-const shader = @import("graphics/shader.zig");
-const mesh = @import("graphics/mesh.zig");
-const texture = @import("graphics/texture.zig");
-const map = @import("world/map.zig");
-const camera = @import("graphics/camera.zig");
-const keyboard = @import("input/keyboard.zig");
-const modelLoader = @import("graphics/model_loader.zig");
+// const window = @import("graphics/window.zig");
+// const shader = @import("graphics/shader.zig");
+// const mesh = @import("graphics/mesh.zig");
+// const texture = @import("graphics/texture.zig");
+// const map = @import("world/map.zig");
+// const camera = @import("graphics/camera.zig");
+// const keyboard = @import("input/keyboard.zig");
+// const modelLoader = @import("graphics/model_loader.zig");
 
 const Vec3 = za.Vec3;
 const Mat4 = za.Mat4;
@@ -36,74 +36,74 @@ pub fn main() !void {
     allocator.initialize();
     defer allocator.terminate();
 
-    window.initialize(800, 600);
-    defer window.terminate();
+    // window.initialize(800, 600);
+    // defer window.terminate();
 
-    shader.initialize();
-    defer shader.terminate();
+    // shader.initialize();
+    // defer shader.terminate();
 
-    stbi.init(allocator.get());
-    defer stbi.deinit();
+    // stbi.init(allocator.get());
+    // defer stbi.deinit();
 
-    mesh.initialize();
-    defer mesh.terminate();
+    // mesh.initialize();
+    // defer mesh.terminate();
 
-    texture.initialize();
-    defer texture.terminate();
+    // texture.initialize();
+    // defer texture.terminate();
 
-    keyboard.initialize();
-    defer keyboard.terminate();
+    // keyboard.initialize();
+    // defer keyboard.terminate();
 
-    shader.new(
-        "main",
-        "shaders/vertex.vert",
-        "shaders/fragment.frag",
-    );
+    // shader.new(
+    //     "main",
+    //     "shaders/vertex.vert",
+    //     "shaders/fragment.frag",
+    // );
 
-    shader.start("main");
+    // shader.start("main");
 
-    // Set up the initial ps1 blockiness.
-    // todo: make this a settings menu element.
-    // window.setPs1Blockiness(40.0);
+    // // Set up the initial ps1 blockiness.
+    // // todo: make this a settings menu element.
+    // // window.setPs1Blockiness(40.0);
 
-    map.load("levels/big_map_test.png");
+    // map.load("levels/big_map_test.png");
 
-    texture.new("textures/sand.png");
+    // texture.new("textures/sand.png");
 
-    texture.use("sand.png");
+    // texture.use("sand.png");
 
-    var rotation: f32 = -30;
-    const translation: f32 = 0;
+    // var rotation: f32 = -30;
+    // const translation: f32 = 0;
 
-    modelLoader.loadModel("models/largemouth.glb");
+    // modelLoader.loadModel("models/largemouth.glb");
 
-    while (!window.shouldClose()) {
-        window.pollEvents();
+    // while (!window.shouldClose()) {
+    //     window.pollEvents();
 
-        camera.setClearColor(0.2, 0.3, 0.3);
-        camera.clearColorBuffer();
-        camera.clearDepthBuffer();
+    //     camera.setClearColor(0.2, 0.3, 0.3);
+    //     camera.clearColorBuffer();
+    //     camera.clearDepthBuffer();
 
-        camera.setCameraPosition(10, 2, 0);
-        camera.setCameraRotation(-20.0, rotation);
-        camera.updateCameraMatrix();
+    //     camera.setCameraPosition(10, 2, 0);
+    //     camera.setCameraRotation(-20.0, rotation);
+    //     camera.updateCameraMatrix();
 
-        rotation += 1;
+    //     rotation += 1;
 
-        camera.updateObjectMatrix(50, 0, translation, 0, 0, 1);
+    //     camera.updateObjectMatrix(50, 0, translation, 0, 0, 1);
 
-        // todo: the map should have a function to draw this lol. This is anarchic.
-        // mesh.draw("ground");
+    //     // todo: the map should have a function to draw this lol. This is anarchic.
+    //     // mesh.draw("ground");
 
-        mesh.draw("model");
+    //     mesh.draw("model");
 
-        if (keyboard.isPressed(glfw.Key.escape)) {
-            window.close();
-        }
+    //     if (keyboard.isPressed(glfw.Key.escape)) {
+    //         window.close();
+    //     }
 
-        window.swapBuffers();
+    //     window.swapBuffers();
 
-        // Make sure this is always last.
-        keyboard._pressReleaseMemoryReset();
-    }
+    //     // Make sure this is always last.
+    //     keyboard._pressReleaseMemoryReset();
+    // }
 }
