2485a638946ac70abb867dec40e038c6c2062b7b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-01-15 04:51:35 -0500

    I have no idea what I'm doing :D

--- src/graphics/model_loader.zig
+++ src/graphics/model_loader.zig
@@ -29,15 +29,51 @@ pub fn loadModel(location: []const u8) void {
     var vertices = std.ArrayList(f32).init(allocator.get());
     defer vertices.deinit();
 
-    for (loader.data.nodes.items) |node| {
-        std.debug.print("Node's name: {s}\nChildren count: {}\nHave skin: {}\nHave mesh: {}\n", .{
-            node.name,
-            node.children.items.len,
-            node.skin != null,
-            node.mesh != null,
-        });
+    if (loader.data.nodes.items.len == 0) {
+        std.log.err("[Model Loader]: Loaded empty model {s}.", .{location});
+        std.process.exit(1);
     }
 
+    // Only loading the first node.
+
+    std.debug.print("{}\n", .{loader.data.nodes.items.len});
+
+    var modelNode: gltf.Node = undefined;
+
+    var found = false;
+
+    for (loader.data.nodes.items) |node| {
+        // I am terrible at using blender so I have to get rid of Empty.
+        if (!std.mem.eql(u8, node.name, "Empty")) {
+            // std.debug.print("HOOPLAH {s}\n", .{node.name});
+            modelNode = node;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        std.log.err("[Model Loader]: Cannot find model {s}.", .{location});
+        std.process.exit(1);
+    }
+
+    var meshIndex: isize = @intCast(modelNode.mesh orelse {
+        std.log.err("[Model Loader]: Cannot find mesh index {s}.", .{location});
+        std.process.exit(1);
+    });
+
+    
+
+    var boof = std.ArrayList(f32).init(allocator.get());
+    loader.getDataFromBufferView(f32, &boof, loader.data.accessors.items[@intCast(meshIndex)], buffer);
+    for (boof.items) |i| {
+        std.debug.print("{}", .{i});
+    }
+
+    boof.clearAndFree();
+    _ = &boof;
+    _ = &meshIndex;
+    _ = &modelNode;
     _ = &loader;
     _ = &buffer;
 }
